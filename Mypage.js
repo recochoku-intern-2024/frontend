1d838ddb3ddce0caa693749d1f64070b34c0cc1d
hisnf3492 <evene141355@gmail.com>
2024-03-24 14:44:34 +0900

    a

--- Mypage.js
+++ Mypage.js
@@ -19,29 +19,28 @@ function getMypage() {
    }
    let  Mypages = getMypage();
 
-   Mypages.forEach((Mypage,index)=>{
-    const tr = document.createElement('tr')
-    const title = document.createElement('td')
-    const author = document.createElement('td')
-    const genre = document.createElement('td')
-    const price = document.createElement('td')
-    title.textContent = Mypage.title;
-    author.textContent = Mypage.author;
- genre.textContent = Mypage.genre;
-    price.textContent = Mypage.price;
-    tr.appendChild(title)
-    tr.appendChild(author)
-    tr.appendChild(genre)
-    tr.appendChild(price)
-    
-    const table =document.getElementById('tbody')
-    table.appendChild(tr)
-   });
+ 
   
-document.getElementById('title').textContent = Mypage[0].title;
 
-document.getElementById('author').textContent = Mypage[0].author;
-
-document.getElementById('genre').textContent = Mypage[0].genre;
-
-document.getElementById('price').textContent = Mypage[0].price;
+axios.get('')
+.then(response => {
+    console.log(response);
+    response.data.forEach((Mypage,index)=>{
+        const tr = document.createElement('tr')
+        const title = document.createElement('td')
+        const author = document.createElement('td')
+        const genre = document.createElement('td')
+        const price = document.createElement('td')
+        title.textContent = Mypage.title;
+        author.textContent = Mypage.author;
+     genre.textContent = Mypage.genre;
+        price.textContent = Mypage.price;
+        tr.appendChild(title)
+        tr.appendChild(author)
+        tr.appendChild(genre)
+        tr.appendChild(price)
+        
+        const table =document.getElementById('tbody')
+        table.appendChild(tr)
+       });
+  });
